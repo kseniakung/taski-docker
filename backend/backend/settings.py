404fc3694ef95b24da5ad2e3d85da522e373137a
Ксения Кунгурова <kseniakung@yandex.ru>
2024-02-29 13:43:31 +0300

    third commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
+    'default': {
         # Меняем настройку Django: теперь для работы будет использоваться
         # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
